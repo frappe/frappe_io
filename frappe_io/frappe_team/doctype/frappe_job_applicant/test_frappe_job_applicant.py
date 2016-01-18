b7e2700f0875e27fffd15135b01281a03511042a
Rushabh Mehta <rmehta@gmail.com>
2016-01-18 09:28:37 +0530

    fix join-us

---
+++ frappe_io/frappe_team/doctype/frappe_job_applicant/test_frappe_job_applicant.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2015, Frappe and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+# test_records = frappe.get_test_records('Frappe Job Applicant')
+
+class TestFrappeJobApplicant(unittest.TestCase):
+	pass
