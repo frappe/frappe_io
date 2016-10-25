4be543876f9dabef88b78e43344eb4a6e58f8667
Rushabh Mehta <rmehta@gmail.com>
2016-10-25 15:12:28 +0530

    new about

---
+++ frappe_io/www/about.py
@@ -0,0 +1,79 @@
+
+def get_context(context):
+	context.team = [
+		{
+			'name': 'Rushabh Mehta',
+			'role': 'Founder and CEO',
+			'twitter': 'rushabh_mehta',
+			'linkedin': 'https://in.linkedin.com/in/rushabhmehta1',
+			'blogger': 'rushabh'
+		},
+		{
+			'name': 'Nabin Hait',
+			'role': 'Chief Operations Officer',
+			'twitter': 'nabinhait',
+			'blogger': 'nabin'
+		},
+		# {
+		# 	'name': 'Pawan Mehta',
+		# 	'role': 'Chief Operations Officer',
+		# 	'linkedin': 'https://in.linkedin.com/in/pawan-mehta-455a179',
+		# 	'blogger': 'pawan'
+		# },
+		{
+			'name': 'Umair Sayed',
+			'role': 'Chief Customer Officer',
+			'twitter': 'umairsTWT',
+			'blogger': 'umair'
+		},
+		{
+			'name': 'Shreyas Patil',
+			'role': 'Developer',
+			'twitter': 'shreyaspatil87',
+			'blogger': 'shreyas'
+		},
+		{
+			'name': 'Kanchan Chauhan',
+			'role': 'Developer',
+			'linkedin': 'https://in.linkedin.com/in/kanchanchauhan1984',
+			'blogger': 'kanchan'
+		},
+		{
+			'name': 'Saurabh Palande',
+			'role': 'Developer',
+			'twitter': 'saurabh6790',
+			'blogger': 'saurabh'
+		},
+		{
+			'name': 'Rohit Waghchaure',
+			'role': 'Developer',
+			'twitter': 'rohitw1991',
+			'blogger': 'rohit'
+		},
+		{
+			'name': 'Neil Lasrado',
+			'role': 'Developer',
+			'twitter': 'neillasrado',
+			'blogger': 'neil'
+		},
+		{
+			'name': 'Faris Ansari',
+			'role': 'Developer',
+			'twitter': 'netchampfaris',
+			'blogger': 'faris'
+		},
+		{
+			'name': 'Arundhati Sharma',
+			'role': 'Support',
+			'blogger': 'arundhati',
+			'linkedin': 'https://in.linkedin.com/in/arundhati-sharma-6311a721'
+		},
+		{
+			'name': 'Prakash Hodage',
+			'role': 'Admin + Accounts',
+			'blogger': 'prakash',
+			'linkedin': 'https://in.linkedin.com/in/prakash-hodage-6702b231'
+		},
+
+
+	]
