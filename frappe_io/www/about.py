442e613416574203f073b35f1133f973dbf4e92e
Suraj Shetty <13928957+surajshetty3416@users.noreply.github.com>
2019-09-12 17:20:01 +0530

    fix: Get team member data from Employee (#204)
    * fix: Get team member data from Employee
    
    * fix: Typo

--- frappe_io/www/about.py
+++ frappe_io/www/about.py
@@ -1,5 +1,7 @@
 import frappe
 
 def get_context(context):
-    context.team_members = frappe.get_all('Team Member',
-		fields=['*'], order_by='joining_date')
+	context.team_members = frappe.get_all('Team Member',
+		fields=['*'],
+		filters={'enabled': 1},
+		order_by='joining_date')
