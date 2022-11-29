3af94eb88cd571c7a10969e8ab62bc7123b7c3e4
Ankush Menat <ankush@frappe.io>
2022-11-29 14:59:07 +0530

    fix: team member fields

--- frappe_io/frappe_team/doctype/team_member/team_member.py
+++ frappe_io/frappe_team/doctype/team_member/team_member.py
@@ -2,14 +2,27 @@
 # Copyright (c) 2018, Frappe and contributors
 # For license information, please see license.txt
 
-from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+
 
 class TeamMember(Document):
 	pass
 
+
 @frappe.whitelist(allow_guest=True)
 def get_team_data():
-	return frappe.get_all('Team Member',
-		fields=['*'], order_by='joining_date')
+	return frappe.get_all(
+		"Team Member",
+		fields=[
+			"name",
+			"image",
+			"social_label",
+			"social_link",
+			"role",
+			"full_name",
+			"employee",
+		],
+		filters={"enabled": 1},
+		order_by="joining_date",
+	)
